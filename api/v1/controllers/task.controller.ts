abafe85f7e447ca3b4e69b6634751c222ceb183d
TaDinhTai <tadinhtai6868@gmail.com>
2024-09-07 22:25:41 +0700

    Cập nhật trạng thái công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,6 +3,7 @@ import Task from "../models/task.model";
 import paginationHelper from "../../../helpers/pagination";
 import searchHelper from "../../../helpers/search";
 
+// [GET] /api/v1/tasks
 export const index = async (req: Request, res: Response) => {
   // Find
   // interface Find {
@@ -77,3 +78,23 @@ export const detail = async (req: Request, res: Response) => {
 
   res.json(task);
 };
+
+// [PATCH] /api/v1/tasks/change-status/:id
+export const changeStatus = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const status: string = req.body.status;
+
+    await Task.updateOne({ _id: id }, { status: status });
+
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công!"
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại!"
+   });
+  }
+}
--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -5,8 +5,9 @@ import * as controller from "../controllers/task.controller";
 
 router.get("/", controller.index);
 
-
 router.get("/detail/:id", controller.detail);
+
+router.patch("/change-status/:id", controller.changeStatus)
  
 
 export const taskRoutes: Router = router; 
